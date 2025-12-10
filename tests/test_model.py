41c1f08998d9cd2f3235af1e4a81c10f67f4ca15
kendallpainley <kendallpainley@gmail.com>
2025-12-09 17:48:14 -0800

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,1 +1,21 @@
 
+import numpy as np
+from src.model import LinearRegressionScratch
+
+def test_normal_equation_fit():
+    X = np.array([[0],[1],[2],[3],[4]], dtype=float)
+    y = 2 + 3*X.ravel()
+    model = LinearRegressionScratch(method="normal")
+    model.fit(X, y)
+    r2 = model.score_r2(X, y)
+    assert r2 > 0.999
+    assert np.allclose(model.theta_.ravel(), [2,3], atol=1e-6)
+
+def test_gradient_descent_fit():
+    X = np.array([[0],[1],[2],[3],[4]], dtype=float)
+    y = 2 + 3*X.ravel()
+    model = LinearRegressionScratch(method="gd", learning_rate=0.05, n_iters=5000)
+    model.fit(X, y)
+    r2 = model.score_r2(X, y)
+    assert r2 > 0.999
+
